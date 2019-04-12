6afad7cde0a39e172806d8cf1915da4f028f3c94
Prafullata Sonawane <prafullatass@gmail.com>
2019-04-12 13:26:56 -0500

    made a list of expensive and inexpensive items

--- Program.cs
+++ Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace ShoppingLinq
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace ShoppingLinq {
+    public class Product {
+        /*
+        Properties
+        */
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        // Constructor method
+        public Product (string title, double price) {
+            this.Title = title;
+            this.Price = price;
+        }
+    }
+    class Program {
+        static void Main (string[] args) {
+            /*
+            We can use curly braces to create instances of objects
+            and immediately inject them into the List.
+        */
+            List<Product> shoppingCart = new List<Product> () {
+                new Product ("Bike", 109.99),
+                new Product ("Mittens", 6.49),
+                new Product ("Lollipop", 0.50),
+                new Product ("Pocket Watch", 584.00)
+            };
+
+            /*
+                IEnumerable is an interface, which we'll get to later,
+                that we're using here to create a collection of Products
+                that we can iterate over.
+            */
+            IEnumerable<Product> inexpensive = from product in shoppingCart
+            where product.Price < 100.00
+            orderby product.Price descending
+            select product;
+            Console.WriteLine("-------- in-Expensive -------------");
+            foreach (Product p in inexpensive) {
+                Console.WriteLine ($"{p.Title} ${p.Price:f2}");
+            }
+
+            /*
+                You can also use `var` when creating LINQ collections. The
+                following variable will still be typed as List<Product> by
+                the compiler, but you don't need to type that all out.
+            */
+            var expensive = from product in shoppingCart
+            where product.Price >= 100.00
+            orderby product.Price descending
+            select product;
+            Console.WriteLine("---------------- Expensive ---------------");
+            foreach (Product p in expensive) {
+                Console.WriteLine ($"{p.Title} ${p.Price:f2}");
+            }
+
         }
     }
 }
